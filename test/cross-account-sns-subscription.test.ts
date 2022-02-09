4a3a2120e005f1a320f244b3eca887fa26036780
Jussi Nupponen <jussi.nupponen@gofore.com>
2022-02-09 09:41:06 +0200

    Initial commit

---
+++ test/cross-account-sns-subscription.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CrossAccountSnsSubscription from '../lib/cross-account-sns-subscription-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cross-account-sns-subscription-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CrossAccountSnsSubscription.CrossAccountSnsSubscriptionStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
